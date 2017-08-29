7c4ffaf40d765631a92e8c1610348638e1fecd11
Katja Durrani <mail@kdurrani.co.uk>
2017-08-29 22:23:33 +0100

    add oncorps and zenstors logo

--- model/sponsor.js
+++ model/sponsor.js
@@ -1,15 +1,19 @@
-var sponsorList = [
+var sponsorList = [  
   {
-    url: "http://www.momentumfinancialtechnology.com/",
-    style: "background-position: -660px 0; width: 120px; height: 31px;" 
+    url: "https://www.ovoenergy.com/",
+    style: "background-position: -920px 0; width: 120px; height: 49px;"
+  },
+  {
+    url: "https://zenstores.workable.com/j/7F7B6D40CE",
+    style: "background-position: -660px 0; width: 120px; height: 36px;" 
+  },
+  {
+    url: "https://oncorps.io",
+    style: "background-position: -660px -58px; width: 120px; height: 39px;" 
   },
   {
     url: "https://www.digitalocean.com/",
     style: "background-position: -790px 0; width: 120px; height: 90px;"
-  },
-  {
-    url: "https://www.ovoenergy.com/",
-    style: "background-position: -920px 0; width: 120px; height: 49px;"
   }
 ];
 
